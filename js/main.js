1586881889cc35e13271791bad5512af83cb2107
Eslam-Hussien <eslam01003733553@gmail.com>
2024-10-12 19:21:22 +0300

    add changes

--- js/main.js
+++ js/main.js
@@ -8,3 +8,19 @@ const sr = ScrollReveal({
 sr.reveal(`.content-data`, {
   interval: 200,
 });
+
+
+// const card = document.querySelector('.card');
+
+// function clickRotate() {
+//   card.classList.toggle('rotated');
+// }
+// card.addEventListener('click', clickRotate);
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('rotated');
+  });
+});
